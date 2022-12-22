051198245239b06b4acce0b783f62b7ea28ad855
Michael Stevenson <michael@mstevenson.net>
2022-12-22 02:50:02 -0800

    Render animation curve and color

---
+++ Editor/Renderers/AnimationCurveRenderer.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityNotebook
+{
+    [UsedImplicitly]
+    public class AnimationCurveRenderer : OutputRendererBase
+    {
+        public override string[] MimeTypes { get; } = { $"{UnityMimePrefix}animationcurve" };
+        public override Type[] SupportedTypes { get; } = { typeof(AnimationCurve) };
+
+        public override void Render(Notebook.CellOutputDataEntry content)
+        {
+            EditorGUILayout.CurveField(content.asset as AnimationCurve, GUILayout.Width(250), GUILayout.Height(120));
+        }
+
+        public override Notebook.CellOutput ObjectToCellOutput(object obj)
+        {
+            return new Notebook.CellOutput
+            {
+                outputType = Notebook.OutputType.DisplayData,
+                data = new List<Notebook.CellOutputDataEntry>
+                {
+                    new()
+                    {
+                        mimeType = MimeTypes[0],
+                        asset = obj
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ Editor/Renderers/ColorRenderer.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityNotebook
+{
+    [UsedImplicitly]
+    public class ColorRenderer : OutputRendererBase
+    {
+        public override string[] MimeTypes { get; } = { $"{UnityMimePrefix}color" };
+        public override Type[] SupportedTypes { get; } = { typeof(Color) };
+
+        public override void Render(Notebook.CellOutputDataEntry content)
+        {
+            EditorGUILayout.ColorField((Color)content.asset, GUILayout.Width(100), GUILayout.Height(100));
+        }
+
+        public override Notebook.CellOutput ObjectToCellOutput(object obj)
+        {
+            return new Notebook.CellOutput
+            {
+                outputType = Notebook.OutputType.DisplayData,
+                data = new List<Notebook.CellOutputDataEntry>
+                {
+                    new()
+                    {
+                        mimeType = MimeTypes[0],
+                        asset = obj
+                    }
+                }
+            };
+        }
+    }
+}
